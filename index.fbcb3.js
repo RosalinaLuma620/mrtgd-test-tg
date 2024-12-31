5bbb3ac38a4dabafdc068cb8aff274b7a5b13b15
RosalinaLuma <rosalinaluma620@gmail.com>
2024-12-31 15:50:51 +0800

    3

--- index.fbcb3.js
+++ index.fbcb3.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.79a88.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
